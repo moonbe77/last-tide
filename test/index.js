1e31baf5f673aead546497840151397f0cbb4619
moonbe77 <moonbe77@gmail.com>
2018-04-20 20:26:11 -0300

    quit test from gitignore

---
+++ test/index.js
@@ -0,0 +1,24 @@
+let nodoMarea = document.querySelector('#marea')
+let nodoTemp = document.querySelector('#temp')
+
+fetch('https://last-tide-pvrmcucimo.now.sh/',{
+    method : "GET",
+    mode: 'cors'
+})
+.then( (response) => {
+    return response.json()
+})
+.then(( response) => {
+    let marea = response    
+    write(marea)
+})
+.catch(err => {
+    console.log(err)
+})
+
+let write = ( m )  => {
+    if (m) {
+        nodoMarea.innerHTML = `Altura: ${m[0].altura} mts`
+        nodoTemp.innerHTML = `Temp: ${m[0].temp}°C`
+    }
+}
